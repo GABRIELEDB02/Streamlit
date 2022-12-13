becacbbcd96db2a534ce9493585e813465aa4cbc
GABRIELE DI BLASIO <77328919+GABRIELEDB02@users.noreply.github.com>
2022-12-13 11:39:51 +0100

    Add files via upload

---
+++ ciao.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# In[14]:
+
+
+st.title('Uber pickups in NYC')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
